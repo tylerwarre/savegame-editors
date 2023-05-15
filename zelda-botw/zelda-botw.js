d05b9abb008912fe457d858f313f82d8e961f190
tylerwarre <tyler@warrens.one>
2023-05-14 19:28:04 -0700

    Added option to add 'n' number of korok seed to map

--- zelda-botw/zelda-botw.js
+++ zelda-botw/zelda-botw.js
@@ -931,6 +931,39 @@ function dist(px,py,pz,l){
 }
 
 
+function addToMapNum(data, icon, num){
+	var px=tempFile.readF32(SavegameEditor.Offsets.PLAYER_POSITION);
+	var py=tempFile.readF32(SavegameEditor.Offsets.PLAYER_POSITION+8);
+	var pz=tempFile.readF32(SavegameEditor.Offsets.PLAYER_POSITION+16);
+
+	var points = [];
+	for (var i = 0; i<data.length; i++){
+		var l = BOTW_Data.COORDS[data[i]]
+		if (l){
+		   points.push({H:data[i], L:l})
+		}
+	}
+	// fill closest first
+	points.sort(function(a,b){
+		aDist = dist(px,py,pz,a.L);
+		bDist = dist(px,py,pz,b.L);
+		return aDist - bDist
+	})
+
+	var count = 0;
+	for (var i = 0; i<points.length && mapPinCount<MAX_MAP_PINS && count<num; i++){
+		var pt = points[i]
+		var hash = pt.H;
+		var offset=SavegameEditor._searchHash(hash);
+		if(offset && !tempFile.readU32(offset + 4)){
+			addMapPin(icon, pt.L)
+			count++;
+			mapPinCount++;
+		}
+	}
+	setValue('number-map-pins', mapPinCount);
+	return count;
+}
 
 function addToMap(data, icon){
 	var px=tempFile.readF32(SavegameEditor.Offsets.PLAYER_POSITION);
@@ -993,8 +1026,8 @@ function addMapPin(icon, location){
 	})
 }
 
-function addKoroksToMap(){
-	var n = addToMap(BOTW_Data.KOROKS, SavegameEditor.Constants.ICON_TYPES.LEAF);
+function addKoroksToMap(num){
+	var n = addToMapNum(BOTW_Data.KOROKS, SavegameEditor.Constants.ICON_TYPES.LEAF, num);
 	MarcDialogs.alert(n+' pins for missing Korok seeds added to map');
 }
 
